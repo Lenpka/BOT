4ac099e1a104ca043028bdbf3fa830b59207d3be
Leonid Konstantinov <leanidkonstantinov@yandex.ru>
2024-09-11 08:08:19 +0300

    virtual

--- create_bot.py
+++ create_bot.py
@@ -1,1 +1,10 @@
-from aiogram import bot
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import os
+import dotenv
+dotenv.load_dotenv()
+storage = MemoryStorage()
+
+bot = Bot(os.getenv('BOT_TOKEN'))
+
+dp = Dispatcher(bot, storage= storage)
